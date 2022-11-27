b6054818d990a83d955f420f76b8b564f7b8e34a
Victor <victortim2002@gmail.com>
2022-11-27 14:08:18 +0300

    3.J (fast ver)

--- 01_part/03_Homework/J_jogging_Manhattan/main.cpp
+++ 01_part/03_Homework/J_jogging_Manhattan/main.cpp
@@ -1,139 +1,64 @@
 #include <iostream>
-#include <set>
-#include <cmath>
+#include <vector>
 
-std::set<std::pair<int, int>> getNewDet(const std::set<std::pair<int, int>> &setLast,
-                                        const int &t, const int &d, const int &x, const int &y) {
-  std::pair<int, int> temp;
-  std::set<std::pair<int, int>> setNew;
-  int maxX = -100000;
-
-  for (auto dot: setLast) {
-    for (int kJ = t; kJ >= 0; --kJ) {
-      temp.first = dot.first + kJ;
-      temp.second = dot.second + (t - kJ);
-      if (std::abs(temp.first - x) + std::abs(temp.second - y) <= d) {
-        if (maxX < temp.first) {
-          maxX = temp.first;
-        }
-        setNew.insert(temp);
-      }
-      temp.first = dot.first - kJ;
-      temp.second = dot.second + (t - kJ);
-      if (std::abs(temp.first - x) + std::abs(temp.second - y) <= d) {
-        setNew.insert(temp);
-      }
-      temp.first = dot.first + kJ;
-      temp.second = dot.second - (t - kJ);
-      if (std::abs(temp.first - x) + std::abs(temp.second - y) <= d) {
-        if (maxX < temp.first) {
-          maxX = temp.first;
-        }
-        setNew.insert(temp);
-      }
-      temp.first = dot.first - kJ;
-      temp.second = dot.second - (t - kJ);
-      if (std::abs(temp.first - x) + std::abs(temp.second - y) <= d) {
-        setNew.insert(temp);
-      }
-    }
-
-    for (int kJ = d; kJ >= 0; --kJ) {
-      temp.first = x + kJ;
-      temp.second = y + (d - kJ);
-      if (std::abs(temp.first - dot.first) + std::abs(temp.second - dot.second) <= t) {
-        if (maxX < temp.first) {
-          maxX = temp.first;
-        }
-        setNew.insert(temp);
-      }
-      temp.first = x - kJ;
-      temp.second = y + (d - kJ);
-      if (std::abs(temp.first - dot.first) + std::abs(temp.second - dot.second) <= t) {
-        setNew.insert(temp);
-      }
-      temp.first = x + kJ;
-      temp.second = y - (d - kJ);
-      if (std::abs(temp.first - dot.first) + std::abs(temp.second - dot.second) <= t) {
-        if (maxX < temp.first) {
-          maxX = temp.first;
-        }
-        setNew.insert(temp);
-      }
-      temp.first = x - kJ;
-      temp.second = y - (d - kJ);
-      if (std::abs(temp.first - dot.first) + std::abs(temp.second - dot.second) <= t) {
-        setNew.insert(temp);
-      }
-    }
-  }
-  bool firstDot = true;
-  bool secondDot = false;
-  int left;
-  int right = maxX;
-
-  for (auto dot: setNew) {
-    if (firstDot) {
-      firstDot = false;
-      left = dot.first;
-    } else {
-      if (temp.first == dot.first) {
-        if (!secondDot) {
-          secondDot = true;
-        } else {
-          if (left != temp.first && right != temp.first) {
-            setNew.erase(temp);
-          }
-        }
-      } else {
-        secondDot = false;
-      }
-    }
-
-    temp = dot;
-  }
-
-  return setNew;
-}
 int main() {
   int t;
   int d;
   int n;
   int x, y;
-  std::set<std::pair<int, int>> setLast;
 
   std::cin >> t >> d >> n;
 
-  std::pair<int, int> temp(0,0);
-  setLast.insert(temp);
+  int maxTxPlusY;
+  int minTxPlusY;
+  int maxTxMinusY;
+  int minTxMinusY;
+
+  int maxDxPlusY;
+  int minDxPlusY;
+  int maxDxMinusY;
+  int minDxMinusY;
+
+  maxTxPlusY = 0;
+  minTxPlusY = 0;
+  maxTxMinusY = 0;
+  minTxMinusY = 0;
 
   for (int kI = 0; kI < n; ++kI) {
     std::cin >> x >> y;
-    setLast = getNewDet(setLast, t, d, x, y);
+
+    maxTxPlusY += t;
+    minTxPlusY -= t;
+    maxTxMinusY += t;
+    minTxMinusY -= t;
+
+    maxDxPlusY = x + y + d;
+    minDxPlusY = x + y - d;
+    maxDxMinusY = x - y + d;
+    minDxMinusY = x - y - d;
+
+    maxTxPlusY = std::min(maxDxPlusY, maxTxPlusY);
+    minTxPlusY = std::max(minDxPlusY, minTxPlusY);
+
+    maxTxMinusY = std::min(maxDxMinusY, maxTxMinusY);
+    minTxMinusY = std::max(minDxMinusY, minTxMinusY);
   }
 
-  bool firstDot = true;
-  std::pair<int, int> innerPoint;
-
-  for (auto dot: setLast) {
-    if (firstDot) {
-      temp = dot;
-      firstDot = false;
-    } else {
-      if (temp.first == dot.first) {
-        for (int kI = temp.second + 1; kI <= dot.second; ++kI) {
-          innerPoint.first = temp.first;
-          innerPoint.second = kI;
-          setLast.insert(innerPoint);
-        }
+  std::pair<int, int> temp;
+  std::vector<std::pair<int, int>> result;
+  for (int plus = minTxPlusY; plus <= maxTxPlusY; ++plus) {
+    for (int minus = minTxMinusY; minus <= maxTxMinusY; ++minus) {
+      temp.first = plus + minus;
+      if (temp.first % 2 == 0) {
+        temp.first /= 2;
+        temp.second = plus - temp.first;
+        result.push_back(temp);
       }
-
-      temp = dot;
     }
   }
 
-  std::cout << setLast.size()<< std::endl;
-  for (auto dot: setLast) {
+  std::cout << result.size() << std::endl;
+  for (auto dot: result) {
     std::cout << dot.first << " " << dot.second << std::endl;
   }
 
